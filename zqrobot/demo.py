057c51ef6b82697280c828d850557d1cf7c1e899
bite_man <bite_man@163.com>
2021-02-01 00:17:49 +0800

    img

--- zqrobot/demo.py
+++ zqrobot/demo.py
@@ -28,7 +28,7 @@ def start(update: Update, context: CallbackContext) -> None:
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    textStr = '![](https://hbimg.huabanimg.com/3bc25c6943564acd0c26ad77101fca1172fa3960a4b85-m3vdYW_fw658/format/webp)' \
+    textStr = '![](https://t7.baidu.com/it/u=874840036,2689408898&fm=193&f=GIF)' \
               'xx担保竭诚为你服务，你可以选择以下方式进行担保'
     update.message.reply_text(textStr, reply_markup=reply_markup,parse_mode=ParseMode.MARKDOWN)
 
